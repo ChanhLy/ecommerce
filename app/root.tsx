809d6d2e37e22f7896a22ad6388299e6cb4eafc6
Chanh Ly <chanhly.vn@gmail.com>
2024-04-20 20:11:15 +0700

    init shadcn
    Signed-off-by: Chanh Ly <chanhly.vn@gmail.com>

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,6 @@
-import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node"; // or cloudflare/deno
-import stylesheet from "~/tailwind.css?url";
+import {Links, Meta, Outlet, Scripts, ScrollRestoration,} from "@remix-run/react";
+import type {LinksFunction} from "@remix-run/node"; // or cloudflare/deno
+import stylesheet from "~/globals.css?url";
 
 export const links: LinksFunction = () => [
     { rel: "stylesheet", href: stylesheet },
@@ -15,7 +9,8 @@ export const links: LinksFunction = () => [
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
+    <head>
+        <title>eCommerce</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
