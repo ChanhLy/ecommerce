24e4464609bd0c386e4a363ab5ca2a328f516c3c
Chanh Ly <chanhly.vn@gmail.com>
2024-04-20 16:48:20 +0700

    add tailwind
    Signed-off-by: Chanh Ly <chanhly.vn@gmail.com>

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -12,24 +12,24 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix (with Vite and Cloudflare)</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://developers.cloudflare.com/pages/framework-guides/deploy-a-remix-site/"
-            rel="noreferrer"
-          >
-            Cloudflare Pages Docs - Remix guide
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+      <div style={{fontFamily: "system-ui, sans-serif", lineHeight: "1.8"}}>
+        <h1 className={"text-red-500"}>Welcome to Remix (with Vite and Cloudflare)</h1>
+        <ul>
+          <li>
+            <a
+                target="_blank"
+                href="https://developers.cloudflare.com/pages/framework-guides/deploy-a-remix-site/"
+                rel="noreferrer"
+            >
+              Cloudflare Pages Docs - Remix guide
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+              Remix Docs
+            </a>
+          </li>
+        </ul>
+      </div>
   );
 }
